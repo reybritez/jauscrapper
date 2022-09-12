28816c8780a3b6cd20fdf2ac46b74ca61c9fc586
Steven Rey Britez <stevenreybritez@gmail.com>
2022-09-12 15:43:19 -0400

    cambios en estilos visuales

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 $('[data-toggle=confirmation]').confirmation({
-  	rootSelector: '[data-toggle=confirmation]',
-	//btnOkLabel: 'Si',
-	//btnCancelLabel: 'No'
+	rootSelector: '[data-toggle=confirmation]',
+  //btnOkLabel: 'Si',
+  //btnCancelLabel: 'No'
 });
