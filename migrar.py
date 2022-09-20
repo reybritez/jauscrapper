6469f47d8aff0e383cd9c2c3cbe6b7fc1249dc63
Steven Rey <stevenreybritez@gmail.com>
2022-09-20 08:00:28 -0400

    Cambios en estructura de tablas

--- migrar.py
+++ migrar.py
@@ -1,21 +1,28 @@
-#La idea de este script es recargar tablas cada que se ejecute
+# La idea de este script es recargar tablas cada que se ejecute
 
-from models import db, Producto
-#from faker import Factory ##Es un rellenador de formularios demo
+from models import db, Producto, Ubicacion, Movimiento, Balance
 
-#fake = Factory.create('es_ES')
+# from faker import Factory ##Es un rellenador de formularios demo
+
+# fake = Factory.create('es_ES')
 
 db.drop_all()
 db.create_all()
 
-#Crear 100 productos de prueba
+# Crear 100 productos de prueba
 
 for i in range(100):
-    nombre = f'Pilsen Lata Pack x {i}'
-    precio = 12000 + (i * 100)
+    nombre_producto = f"Pilsen Lata Pack x {i}"
+    detalles_producto = f"Detalle {i}"
+    cantidad_producto = 1 + (i * 10)
 
-    #Guardar en la base de datos
-    producto_unitario = Producto(nombre= nombre, precio = precio)
+    # Guardar en la base de datos
+    producto_unitario = Producto(
+        nombre_producto=nombre_producto,
+        detalles_producto=detalles_producto,
+        cantidad_producto=cantidad_producto,
+    )
     db.session.add(producto_unitario)
+
 
 db.session.commit()
--- models.py
+++ models.py
@@ -1,18 +1,60 @@
+from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app= Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
 
 class Producto(db.Model):
 
-    __tablename__ = 'producto'
+    __tablename__ = "producto"
 
-    id = db.Column(db.Integer, primary_key = True)
-    nombre = db.Column(db.String(80), nullable = False)
-    precio = db.Column(db.Integer, nullable = False)
+    id_producto = db.Column(db.Integer, primary_key=True)
+    nombre_producto = db.Column(db.String(80), nullable=False)
+    detalles_producto = db.Column(db.String(120), nullable=False)
+    cantidad_producto = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return '<Productos %r' % self.nombre
+        return "Productos %r" % self.nombre_producto
+
+
+class Ubicacion(db.Model):
+
+    __tablename__ = "ubicacion"
+
+    id_ubicacion = db.Column(db.Integer, primary_key=True)
+    nombre_ubicacion = db.Column(db.String(20), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Ubicacion('{self.id_ubicacion}', '{self.nombre_ubicacion}'"
+
+
+class Movimiento(db.Model):
+
+    __tablename__ = "movimiento"
+
+    id_movimiento = db.Column(db.Integer, primary_key=True)
+    hora_fecha_movimiento = db.Column(db.DateTime, default=datetime.utcnow())
+    salida_desde = db.Column(db.String(20), nullable=False)
+    entrada_a = db.Column(db.String(20), nullable=False)
+    nombre_producto_a_mover = db.Column(db.String(20), nullable=False)
+    cantidad_a_mover = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Movimiento('{self.hora_fecha_movimiento}', '{self.salida_desde}','{self.entrada_a}','{self.nombre_producto_a_mover}','{self.cantidad_a_mover}'"
+
+
+class Balance(db.Model):
+
+    __tablename__ = "balance"
+
+    id_balance = db.Column(db.Integer, primary_key=True)
+    nombre_producto = db.Column(db.String(20), nullable=False)
+    ubicacion_producto = db.Column(db.String(20), nullable=False)
+    cantidad_producto = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"('{self.nombre_producto}','{self.ubicacion_producto}','{self.cantidad_producto}')"
