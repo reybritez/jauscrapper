294d55101df770ca0bc5947fab2a97b194028ac5
Steven Rey <stevenreybritez@gmail.com>
2022-12-21 10:58:10 -0300

    añadi form para movimientos

--- formularios.py
+++ formularios.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, SelectField
 from wtforms.validators import DataRequired, Length, NumberRange
-
 
 class FormularioProducto(FlaskForm):
     nombre_producto = StringField(
@@ -34,5 +33,24 @@ class FormularioUbicacion(FlaskForm):
         validators=[
             DataRequired(),
             Length(min=-1, max=80, message="No puede tener más de 80 caracteres"),
+        ],
+    )
+
+class MoverProducto(FlaskForm):
+    nombre_producto_a_mover = SelectField(
+        'Product Name'
+    )
+    salida_desde = SelectField(
+        'Source'
+    )
+    entrada_a = SelectField(
+        'Destination'
+    )
+    cantidad_a_mover = IntegerField(
+        'Quantity',
+        validators=[
+            NumberRange(
+                min=1, max=10000000, message="No se puede mover más 1 millón de productos al mismo tiempo"
+            )
         ],
     )
